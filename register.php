618009e97d5425b80438bd8ac06c7dcc07953450
Dii ez <127297402+Rekmast@users.noreply.github.com>
2023-10-21 11:07:39 +0700

    pindahin login ,register

---
+++ register.php
@@ -0,0 +1,20 @@
+<div class="signup-form">
+                    <div class="title">Signup</div>
+                    <form method="post" action="register_process.php">
+                        <div class="input-boxes">
+                            <div class="input-box">
+                                <i class="fa-solid fa-user-plus"></i>
+                                <input type="text" name="username" placeholder="Enter your name" required>
+                            </div>
+                            <div class="input-box">
+                                <i class="fas fa-lock"></i>
+                                <input type="password" name="password" placeholder="Enter your password" required>
+                            </div>
+                            <div class="button input-box">
+                                <input type="submit" value="Sumbit">
+                            </div>
+                            <div class="text sign-up-text">Already have an account? <label for="flip">Login now</label>
+                            </div>
+                        </div>
+                    </form>
+                </div>
